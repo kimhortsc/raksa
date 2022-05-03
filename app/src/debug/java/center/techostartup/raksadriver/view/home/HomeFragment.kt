504dfdbe3feeabcf0b7ded51f6594114373c2136
Kimhor Mong <kimhormong3@gmail.com>
2022-05-03 15:24:53 +0700

    add requesting enable location on device

--- app/src/debug/java/center/techostartup/raksadriver/view/home/HomeFragment.kt
+++ app/src/debug/java/center/techostartup/raksadriver/view/home/HomeFragment.kt
@@ -2,17 +2,20 @@ package center.techostartup.raksadriver.view.home
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.pm.PackageManager
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender.SendIntentException
 import android.location.Location
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import center.techostartup.raksadriver.R
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -20,7 +23,9 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.Task
-import org.xmlpull.v1.sax2.Driver
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,22 +37,16 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment(), OnMapReadyCallback {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+class HomeFragment : Fragment(), OnMapReadyCallback, EasyPermissions.PermissionCallbacks {
 
     private lateinit var mMap: GoogleMap
     private lateinit var supportMapFragment: SupportMapFragment
     private lateinit var client: FusedLocationProviderClient;
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val PERMISSION_LOCATION_REQUEST_CODE = 1
+    private val REQUEST_CHECK_SETTING = 1
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -61,24 +60,34 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // enable location on device
+        enableLocation()
 
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         supportMapFragment =  childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         client = LocationServices.getFusedLocationProviderClient(requireActivity())
 
-
-        // check permission
-        if(ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED) {
-            fetchLocation()
-        } else{
-            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 44)
-        }
-
+        supportMapFragment.getMapAsync(this)
     }
 
 
     override fun onMapReady(p0: GoogleMap) {
+        if(hasLocationAccess()) {
+            fetchLocation()
+        } else{
+            requestLocationPermission()
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        if(EasyPermissions.permissionPermanentlyDenied(this, perms.first())){
+            SettingsDialog.Builder(requireActivity()).build().show()
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
         fetchLocation()
     }
 
@@ -88,24 +97,16 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if(requestCode == 44){
-            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                fetchLocation()
-            }
-        }
-
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 
     @SuppressLint("MissingPermission")
     private fun fetchLocation() {
         val task: Task<Location> = client.lastLocation
-
         task.addOnSuccessListener { location ->
             if (location != null) {
                 supportMapFragment.getMapAsync{ map ->
                     val latLng = LatLng(location.latitude, location.longitude)
-
                     val marker = MarkerOptions().position(latLng).title("Here I am")
                     map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15F))
                     map.addMarker(marker)
@@ -114,4 +115,77 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
+    private fun hasLocationAccess() =
+        EasyPermissions
+            .hasPermissions(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+
+    private fun requestLocationPermission() =
+        EasyPermissions
+            .requestPermissions(
+                this,
+                "Map cannot work without location permission",
+                PERMISSION_LOCATION_REQUEST_CODE,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+
+
+    private fun enableLocation() {
+        val locationRequest: LocationRequest = LocationRequest.create()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 30 * 1000
+        locationRequest.fastestInterval = 5 * 1000
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+        builder.setAlwaysShow(true)
+
+        val result = LocationServices.getSettingsClient(requireActivity()).checkLocationSettings(builder.build())
+
+         result.addOnCompleteListener { task ->
+            try {
+                val response = task.getResult(ApiException::class.java)
+                Toast.makeText(requireContext(), "GPS is enable", Toast.LENGTH_SHORT).show()
+                // All location settings are satisfied. The client can initialize location
+                // requests here.
+
+            } catch (exception: ApiException) {
+                when (exception.statusCode) {
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED ->
+                        // Location settings are not satisfied. But could be fixed by showing the
+                        // user a dialog.
+                        try {
+
+                            // Cast to a resolvable exception.
+                            val resolvable = exception as ResolvableApiException
+
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            resolvable.startResolutionForResult(
+                                requireActivity(),
+                                REQUEST_CHECK_SETTING
+                            )
+                        } catch (e: SendIntentException) {
+                            // Ignore the error.
+                        } catch (e: ClassCastException) {
+                            // Ignore, should be an impossible error.
+                        }
+                    LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {}
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQUEST_CHECK_SETTING){
+            when (resultCode){
+                Activity.RESULT_OK -> Toast.makeText(requireContext(), "GPS is enable", Toast.LENGTH_SHORT).show()
+                Activity.RESULT_CANCELED -> Toast.makeText(requireContext(), "GPS is required", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
 }
+
